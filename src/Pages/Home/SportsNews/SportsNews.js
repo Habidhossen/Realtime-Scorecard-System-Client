3bab19330a8c2763acc2432a37e419391a9d53a5
Habidhossen <habidhossen2@gmail.com>
2023-02-25 02:37:24 +0600

    footer addedf

--- src/Pages/Home/SportsNews/SportsNews.js
+++ src/Pages/Home/SportsNews/SportsNews.js
@@ -1,4 +1,13 @@
-import { Container, Grid, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 const SportsNews = () => {
@@ -12,8 +21,72 @@ const SportsNews = () => {
     >
       <Typography variant="h6">Latest Sports News</Typography>
       <Grid container rowSpacing={1}>
-        <Grid xs={12} md={6} lg={6}></Grid>
-        <Grid xs={12} md={6} lg={6}></Grid>
+        <Grid xs={12} md={6} lg={4}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              sx={{ height: 140 }}
+              image="https://preview.colorlib.com/theme/soccer/images/bg_3.jpg.webp"
+              title="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                News headline!
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
+                quam molestiae sed necessitatibus explicabo itaque aspernatur
+                obcaecati minima expedita consectetur!
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Read More</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid xs={12} md={6} lg={4}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              sx={{ height: 140 }}
+              image="https://preview.colorlib.com/theme/soccer/images/bg_3.jpg.webp"
+              title="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                News headline!
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
+                quam molestiae sed necessitatibus explicabo itaque aspernatur
+                obcaecati minima expedita consectetur!
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Read More</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid xs={12} md={6} lg={4}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              sx={{ height: 140 }}
+              image="https://preview.colorlib.com/theme/soccer/images/bg_3.jpg.webp"
+              title="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                News headline!
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
+                quam molestiae sed necessitatibus explicabo itaque aspernatur
+                obcaecati minima expedita consectetur!
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Read More</Button>
+            </CardActions>
+          </Card>
+        </Grid>
       </Grid>
     </Container>
   );
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,15 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 const Footer = () => {
+  const year = new Date().getFullYear();
+
   return (
-    <div>
-      <h1>This is Footer</h1>
-    </div>
+    <Box my={3}>
+      <Typography component="p" align="center">
+        Copyright © {year} Habid Hossen. All rights reserved.
+      </Typography>
+    </Box>
   );
 };
 
