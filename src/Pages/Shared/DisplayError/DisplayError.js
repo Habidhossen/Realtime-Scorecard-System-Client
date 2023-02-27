565f521b50d9eb808ae4a84ee80af01174dfff20
Habidhossen <habidhossen2@gmail.com>
2023-02-28 02:17:17 +0600

    Display eror page updated

--- src/Pages/Shared/DisplayError/DisplayError.js
+++ src/Pages/Shared/DisplayError/DisplayError.js
@@ -1,10 +1,36 @@
+import { Button, Container, Typography } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const DisplayError = () => {
   return (
-    <div>
-      <h1>Not Found!</h1>
-    </div>
+    <Container
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+    >
+      <Typography variant="h1">404</Typography>
+      <Typography variant="h5" style={{ marginTop: "16px" }}>
+        Page not found
+      </Typography>
+      <Typography variant="body1" style={{ marginTop: "16px" }}>
+        The page you are looking for might have been removed or is temporarily
+        unavailable.
+      </Typography>
+      <Button
+        component={Link}
+        to="/"
+        variant="contained"
+        color="primary"
+        style={{ marginTop: "16px" }}
+      >
+        Go to Home
+      </Button>
+    </Container>
   );
 };
 
